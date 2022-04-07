7178b6727e0a06dd843a1251750372cdb7438de4
Otaye Anastasia <otayeanastasia@gmail.com>
2022-04-07 08:49:52 -0700

    Writing my first C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,11 @@
 /**
  * main - main function starts here
  * puts - print a line of text
- * return -return 0
+ * return - return 0
+ * Return: 0
 */
 int main(void) 
 {
-puts ("\"Programming is like building a multilingual puzzle");
-return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
