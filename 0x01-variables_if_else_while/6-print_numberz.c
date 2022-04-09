186abc65611386eb21c43ae158f703ab722eef2a
Otaye Anastasia <otayeanastasia@gmail.com>
2022-04-09 08:22:09 -0700

    Printing numbers with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - function to print first 10 numbers
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+
+	return (0);
+}
