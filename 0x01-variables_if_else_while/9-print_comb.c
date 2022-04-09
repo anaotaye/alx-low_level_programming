ed14a4868c9d2f437e48fc45ef54940e73f1457f
Otaye Anastasia <otayeanastasia@gmail.com>
2022-04-09 09:58:57 -0700

    Printing all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,18 +2,21 @@
 
 /**
  * main - function to print numbers followed by a comma and space
- * Return: 0(success)
+ * Return: 0 (success)
  */
 
 int main(void)
 {
-	int num = '0';
+	int num;
 
 	for (num = '0'; num <= '9'; num++)
 	{
 		putchar(num);
+		if (num != 9)
+		{
 		putchar(',');
 		putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
