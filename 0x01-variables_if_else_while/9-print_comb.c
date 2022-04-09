aa4d842d254da3c4765c08f476c5b2b9c70de531
Otaye Anastasia <otayeanastasia@gmail.com>
2022-04-09 10:09:18 -0700

    Printing all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,10 +12,10 @@ int main(void)
 	for (num = '0'; num <= '9'; num++)
 	{
 		putchar(num);
-		if (num != 9)
+		if (num != '9')
 		{
-		putchar(',');
-		putchar(' ');
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
