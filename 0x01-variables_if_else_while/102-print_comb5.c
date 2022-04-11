02ac07e5e5f237d62fe076c5228c15ba5984d1cb
Otaye Anastasia <otayeanastasia@gmail.com>
2022-04-11 12:56:23 -0700

    Printing two two-digit number combinations

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,28 +7,24 @@
 
 int main(void)
 {
-	int num1, num2, num3, num4;
+	int num1, num2;
 
-	for (num1 = '0'; num1 <= '9'; num1++)
+	for (num1 = 0; num1 <= 99; num1++)
 	{
-		for (num2 = '0'; num2 <= '9'; num2++)
+		for (num2 = 0; num2 <= 99; num2++)
 		{
-			for (num3 = '0'; num3 <= '9'; num3++)
+			if (num1 < num2)
 			{
-				for (num4 = '1'; num4 <= '9'; num4++)
-				{
-					putchar(num1);
-					putchar(num2);
-					putchar(' ');
-					putchar(num3);
-					putchar(num4);
+				putchar((num1 / 10) + 48);
+				putchar((num1 % 10) + 48);
+				putchar(' ');
+				putchar((num2 / 10) + 48);
+				putchar((num2 % 10) + 48);
 
-					if ((num1 != '9' || num2 != '8') || (num3 != '9' || num4 != '9'))
-		
-					{
-						putchar(',');
-						putchar(' ');
-					}
+				if (num1 != 98 || num2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
