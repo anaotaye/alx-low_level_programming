44b46bdebe6e7544ba4b741f8930cfa1f5923229
Otaye Anastasia <otayeanastasia@gmail.com>
2022-04-12 13:52:29 -0700

    Determining the case of a letter

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _islower - function to determine the case of a letter
+ * @c - the character to be checked
+ *
+ * Return: returns 1 if c is lowercase and 0 if not
+ */
+
+int _islower(int c)
+{
+	if (islower(c) != 0)
+		return (1);
+	else
+		return (0);
+}
