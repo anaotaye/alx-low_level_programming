613eb3cf4537592cc852b9d3891d203960b305db
Otaye Anastasia <otayeanastasia@gmail.com>
2022-04-12 02:09:43 -0700

    Printing putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - function to print a string
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - function that uses another function to print alphabets
+ * print_alphabet - function to print alphabets
+ *
+ * Return: 0 (success)
+ */
+
+void print_alphabet(void);
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	char alpha = 'a';
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha)
+	}
+	putchar('\n');
+	return (0);
+}	
