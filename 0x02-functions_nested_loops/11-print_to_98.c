57d573ce6deb57a55efdfaf5168e9d5759fbb0e3
Otaye Anastasia <otayeanastasia@gmail.com>
2022-04-13 03:01:25 -0700

    Printing all natural numbers to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,7 +14,7 @@ void print_to_98(int n)
 	{
 		for (n = n; n <= 98; n++)
 		{
-			_putchar(n);
+			_putchar(n + '0');
 			_putchar(',');
 			_putchar(' ');
 		}
@@ -23,7 +23,7 @@ void print_to_98(int n)
 	{
 		for (n = n; n >= 98; n--)
 		{
-			_putchar(n);
+			_putchar(n + '0');
 			_putchar(',');
 			_putchar(' ');
 		}
