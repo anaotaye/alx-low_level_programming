ee7413196b76b2dd4eeba4994ff4040318450dcc
Otaye Anastasia <otayeanastasia@gmail.com>
2022-04-13 01:04:38 -0700

    Computing the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _abs - function to return the absolute value of a number
+ *
+ * Return: 0 (success)
+ */
+
+int _abs(int)
+{
+	return (abs(int));
+	return (0);
+}
