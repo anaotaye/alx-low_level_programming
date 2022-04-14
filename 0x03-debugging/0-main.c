0d264d26854ff9baebd8b94c5e6158d24c932114
Otaye Anastasia <otayeanastasia@gmail.com>
2022-04-14 01:12:17 -0700

    Testing a program that checks for positivity or negativity

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * main - tests function that checks if an integer is positive or negative
- *
- * Return: 0 (success)
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
 
 int main(void)
