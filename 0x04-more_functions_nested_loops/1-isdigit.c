26f75ed243362d5902f3ed67175483da15a7b967
Otaye Anastasia <otayeanastasia@gmail.com>
2022-04-15 16:06:31 -0700

    Multiplying two integers

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -20,4 +20,3 @@ int _isdigit(int c)
 		return (0);
 	}
 }
-}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - function to multiply two numbers
+ * @a: first number to multiply
+ * @b: second number to multiply
+ *
+ * Return: 0 (success)
+ */
+
+int mul(int a, int b)
+{
+	int mult;
+
+	mult = a * b;
+	return (0);
+}
