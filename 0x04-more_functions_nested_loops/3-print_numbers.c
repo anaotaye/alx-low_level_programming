cfbf03db1e414670dbcbf8a6ccf4399b45ec03a5
Otaye Anastasia <otayeanastasia@gmail.com>
2022-04-15 16:14:33 -0700

    Function to multiply two numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * print_numbers - funtion to print numbers
+ * 
+ * Return: 0 (success)
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <=9; i++)
+	{
+		_putchar(i);
+	}
+	return (0);
+}
