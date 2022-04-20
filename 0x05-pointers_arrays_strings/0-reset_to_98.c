434050838f8dc882ef7e30c571c09c013020ee42
Otaye Anastasia <otayeanastasia@gmail.com>
2022-04-20 13:08:44 -0700

    Using pointer to update a value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Function to take a pointer to an int and update the value
+ * @n: pointer to be updated
+ *
+ * Return: no return
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains C programs using pointers, arrays and strings
