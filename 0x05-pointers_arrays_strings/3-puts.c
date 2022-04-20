8240623c44983e01804f982be8688b3b9cbe7edd
Otaye Anastasia <otayeanastasia@gmail.com>
2022-04-20 13:34:16 -0700

    Using puts to print a string to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - Function to print string to stdout
+ * @str: string to print to stdout
+ *
+ * Return: nothing to return
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+	_putchar('\n');
+}
