45036a38dad1c591ed5d99736c7164b99f2af84f
Otaye Anastasia <otayeanastasia@gmail.com>
2022-04-24 14:19:25 -0700

    Changing all lowercase to upercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - function to change lowercase letters to uppercase
+ * @str: string to operate on
+ *
+ * Return: changed string
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	while (str[i] != 0; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+		i++;
+	}
+	return (s);
+}
