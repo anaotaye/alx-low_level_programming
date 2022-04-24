d4d1482c0b2c55d0f818bfa26b58882aafa1812f
Otaye Anastasia <otayeanastasia@gmail.com>
2022-04-24 12:41:52 -0700

    Copying a string into another string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy - function that copies a string
+ * @dest: pointer to value to be copied into
+ * @src: pointer to value to be copied
+ * @n: number of chracters to be copied
+ *
+ * Return: return the copied string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	return(strncpy(dest, src, n));
+}
