4678c8c19a490e2cc82041641dec9a2d2c9f97d7
Otaye Anastasia <otayeanastasia@gmail.com>
2022-04-24 14:30:01 -0700

    Changing all lowercase to upercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,11 +11,10 @@ char *string_toupper(char *str)
 {
 	int i;
 
-	while (str[i] != 0)
+	for (i = 0; str[i] != 0; i++)
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 			str[i] -= 32;
-		i++;
 	}
 	return (str);
 }
