1c4a3c6c20712ed93cc91e69666597d20c58b1b9
Otaye Anastasia <otayeanastasia@gmail.com>
2022-04-25 01:08:41 -0700

    Capitalizing all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,8 +18,8 @@ char *cap_string(char *str)
 			str[0] = str[0] - 32;
 		for (j = 0; symbols[j] != '\0'; j++)
 		{
-			if(str[i] == symbols[j] && str[i + 1] >= 97 str[i + 1] <= 122)
-				s[i + 1] = s[i + 1] - 32;
+			if (str[i] == symbols[j] && str[i + 1] >= 97 str[i + 1] <= 122)
+				str[i + 1] = str[i + 1] - 32;
 		}
 	}
 	return (str);
