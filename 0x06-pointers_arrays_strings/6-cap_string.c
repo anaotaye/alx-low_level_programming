ae0fbb76734c77c9667afdfd2b7b5a8081aa49b0
Otaye Anastasia <otayeanastasia@gmail.com>
2022-04-25 02:09:51 -0700

    Converting all first letters of each word of a sentence to uppercase

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,13 +14,22 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[0] >= 97 && str[0] <= 122)
-			str[0] = str[0] - 32;
-		for (j = 0; symbols[j] != '\0'; j++)
+		if (i == 0)
 		{
-			if (str[i] == symbols[j] && str[i + 1] >= 97 str[i + 1] <= 122)
-				str[i + 1] = str[i + 1] - 32;
+			if(str[i] >= 'a' && str[i] <= 'z')
+				str[i] = str[i] - 32;
+			continue;
+		}
+		if (str[i] == symbols[i])
+		{
+			++i;
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32;
+				continue;
+			}
 		}
 	}
+
 	return (str);
 }
