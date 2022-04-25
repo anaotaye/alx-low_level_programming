96c6923e7e6e9ff0b5c776a5cff54cb21a37d510
Otaye Anastasia <otayeanastasia@gmail.com>
2022-04-25 06:06:35 -0700

    Encoding letters with rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * rot13 - function to encode a string using rot13
+ * @str: pointer to string to encode
+ *
+ * Return: pointer to encoded string
+ */
+
+char *rot13(char *str)
+{
+	int i, j;
+	char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (alpha[j] != '\0')
+		{
+			if (str[i] == alpha[j])
+			{
+				str[i] = rot[j];
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (str);
+}
