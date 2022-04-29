0de79de1e61238ac5cbd94d191af460c251d4fdc
Anastasia <otayeanastasia@gmail.com>
2022-04-28 22:48:52 -1100

    Calculating the power ofa number with recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _pow_recursion - Function that returns the value of x raised to the powerof y
+ * _pow_recursion - Function to calculate x raised to the power of y
  * @x: base
  * @y: exponent
  *
