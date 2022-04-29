6f88a47477a7bad6d032539cee8de99343f65ff8
Anastasia <otayeanastasia@gmail.com>
2022-04-28 23:03:11 -1100

    Calculating the natural square root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ * _sqrt_recursion - calculates the natural square root of a number
+ * @n: number to be used
+ *
+ * Return: returns the result
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (sqrt(n));
+}
