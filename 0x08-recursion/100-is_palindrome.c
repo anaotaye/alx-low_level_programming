d80eed0b2e89df8d3ca4163cbf4f9c189550179f
Anastasia <otayeanastasia@gmail.com>
2022-04-29 00:03:50 -1100

    Checkimg if a number is a prime number

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _strlen - claculate length of string
+ * @s: pointer to string
+ *
+ * Return: return length of string
+ */
+
+int _strlen(char *s)
+{
+	if (!*s)
+		return (0);
+	return (1 + _strlen(++s));
+}
+
+
+/**
+ * comparator - compare strings
+ * @s: pointer to string
+ * @i: index
+ *
+ * Return: return integer value
+ */
+
+int comparator(char *s, int i)
+{
+	if (i < 1)
+		return (1);
+	if (*s == *(s + i))
+		return (comparator(s + 1, i - 2));
+	return (0);
+}
+
+
+/**
+ * is_palindrome - function to check if a string is a palindrome
+ * @s: pointer to string
+ *
+ * Return: returns 1 if it's a palindrome and 0 if not
+ */
+
+int is_palindrome(char *s)
+{
+	int length = _strlen(s);
+
+	return(comparator(s, length - 1));
+}
+
