9e310ec1570408361179ab610b63efdb3d974f5f
Anastasia <otayeanastasia@gmail.com>
2022-05-02 12:20:41 -1100

    Program to print the result of arguments passed to main function

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that multiplies two numbers
+ * @argc: length of argv array
+ * @argv: pointer to array of command line arguments
+ *
+ * Return: 0(success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, mul = 1;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+			mul *= atoi(argv[i]);
+	}
+	printf("%d\n", mul);
+	return (0);
+}
