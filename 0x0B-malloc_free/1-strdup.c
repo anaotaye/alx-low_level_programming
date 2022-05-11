eacc2f436c04609d84474002e8992556b2a19576
Otaye Anastasia <otayeanastasia@gmail.com>
2022-05-11 11:42:03 -0700

    Copying  string using malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+
+/**
+ * _strdup - Function to return a pointer to a space in memory
+ * @str: ponter to copy of string in allocated memory
+ *
+ * Return: NULL if string is NULL or returns pointer to new string
+ */
+
+char *_strdup(char *str)
+{
+	char *s;
+	int i = 0, j;
+
+	if (!str)
+		return (NULL);
+
+	while (*(str + i))
+		i++;
+
+	s = malloc(i * sizeof(char));
+
+	if (str == NULL)
+		return (NULL);
+
+	for (j = 0; j <= i; j++)
+		s[j] = str[j];
+
+	return (s);
+}
---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: return pointer or NUL if failed
+ */
+
+char *str_concat(char *s1, char 2)
+{
