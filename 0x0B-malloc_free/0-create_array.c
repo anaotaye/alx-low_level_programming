2f2954aaac621bfc543051ddf490a58738f64d68
Otaye Anastasia <otayeanastasia@gmail.com>
2022-05-11 08:54:04 -0700

    Adding files

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (0);
 
-	p = malloc((size) * sizeof(char));
+	ptr = malloc((size) * sizeof(char));
 
 	if (!p)
 		return (0);
