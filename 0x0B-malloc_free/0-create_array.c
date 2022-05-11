64f51e0d854d111fb03d4221851066e06a45214d
Otaye Anastasia <otayeanastasia@gmail.com>
2022-05-11 08:47:57 -0700

    Adding files

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - Function to create an array of chars using malloc
+ * @size: size of array
+ * @c: char to initialize with
+ *
+ * Return: NULL if size is 0 and a pointer if NULL fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i;
+
+	if (size == 0)
+		return (0);
+
+	p = malloc((size) * sizeof(char));
+
+	if (!p)
+		return (0);
+	for (i = 0; i < size; i++)
+		p[i] = c;
+	return (p);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+This directory contains programs demonstrating malloc and free
