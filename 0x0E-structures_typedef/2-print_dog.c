81bdf33b5f0e506cbf99a7c8c42e1a276c7c1fc5
Otaye Anastasia <otayeanastasia@gmail.com>
2022-05-12 08:05:38 -0700

    Printing a struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints name of dog, it's age and owner if not NULL
+ * @d: pointer to struct dog
+ *
+ * Return: nothing to return
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+
+	printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("(nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
+}
