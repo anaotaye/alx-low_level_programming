1ff77735797887da3e728afbddd0001c0c8cad4a
Otaye Anastasia <otayeanastasia@gmail.com>
2022-05-12 02:37:53 -0700

    Freeing memory of previously allocated two dimensionl array

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - function that frees previously created two dimensional array
+ * @grid: grid to free
+ * @height: columns
+ *
+ * Return: nothing to return
+ */
+
+vod free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid != NULL || height != 0)
+	{
+		for (i = 0; i < height; i++)
+			free(grid[i]);
+		free(grid);
+	}
+}
