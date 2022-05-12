7c72450ea392315e77b80b49e428d24bdeef235b
Otaye Anastasia <otayeanastasia@gmail.com>
2022-05-12 03:15:48 -0700

    Using malloc to allocate memory for concatenating all arguments of the program

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,7 +11,7 @@
 
 char *argstostr(int ac, int **av)
 {
-	int i, n, k = 0, length = 0;
+	int i, j, n, k = 0, length = 0;
 	char *str;
 
 	if (ac == 0 || av == NULL)
@@ -19,25 +19,28 @@ char *argstostr(int ac, int **av)
 
 	for (i = 0; i < ac; i++)
 	{
-		for (n = 0; av[i][n]; n++)
-			length++;
+		n = 0;
+		while (av[i][n])
+			n++;
+		length += n + 1;
 	}
-	length += ac;
 
-	str = malloc(sizeof(char) * length + 1);
-	if (str == NULL)
+	str = malloc((length + 1) * sizeof(char));
+	if (str == 0)
 		return (NULL);
 
-	for (i =  0; i < ac; i++)
+	for (j = 0; j < ac; j++)
 	{
-		for (n = 0; av[i][n]; n++)
+		n = 0;
+		while (av[j][n])
 		{
-			str[k] = av[i][n];
+			*(str + k) = av[j][n];
 			k++;
+			n++;
 		}
-		if (str[k] == '\0')
-			str[k++] = '\n';
+		*(str + k) = '\n';
+		k++;
 	}
-
+	*(str + k) = '\0';
 	return (str);
 }
