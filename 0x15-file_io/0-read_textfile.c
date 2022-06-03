4dc457f427e6f229b6feba1f70bea69c336111ac
Otaye Anastasia <otayeanastasia@gmail.com>
2022-06-03 08:46:05 -0700

    Adding programs

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -28,7 +28,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	rd = read(fd, buf, letters);
-	wr = writ(STDOUT_FILENO, buf, rd);
+	wr = write(STDOUT_FILENO, buf, rd);
 
 	close(fd);
 
