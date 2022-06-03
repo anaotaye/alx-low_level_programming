8880d9766016092a537effb2381ee353f954c6a6
Otaye Anastasia <otayeanastasia@gmail.com>
2022-06-03 08:42:36 -0700

    Adding programs

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prnts it to the standard output
+ * @filename: pointer to name of file
+ * @letters: number of letters to read and print
+ *
+ * Return: number of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t rd, wr;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+
+	if (!buf)
+		return (0);
+
+	rd = read(fd, buf, letters);
+	wr = writ(STDOUT_FILENO, buf, rd);
+
+	close(fd);
+
+	free(buf);
+
+	return (wr);
+}
